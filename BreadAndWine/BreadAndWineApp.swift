3a61ed8b5d942b919f92258db372138e8682b2c7
Anne Ezurike <annie.ezc@gmail.com>
2025-03-29 15:48:27 +0100

    Initial Commit

---
+++ BreadAndWine/BreadAndWineApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreadAndWineApp.swift
+//  BreadAndWine
+//
+//  Created by Anne Ezurike on 29.03.25.
+//
+
+import SwiftUI
+
+@main
+struct BreadAndWineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreadAndWineTests/BreadAndWineTests.swift
@@ -0,0 +1,17 @@
+//
+//  BreadAndWineTests.swift
+//  BreadAndWineTests
+//
+//  Created by Anne Ezurike on 29.03.25.
+//
+
+import Testing
+@testable import BreadAndWine
+
+struct BreadAndWineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
